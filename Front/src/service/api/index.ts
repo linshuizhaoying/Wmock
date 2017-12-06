693f76318dc5e6c8c06d320399a411b856985f2d
linshuizhaoying <4799109@qq.com>
2017-12-06 20:39:40 +0800

    fix deploy

--- Front/src/service/api/index.ts
+++ Front/src/service/api/index.ts
@@ -1,6 +1,6 @@
 // 定义测试接口地址
 // const baseUrl = "http://new.haoqiao.me:8866"
-const baseUrl = "http://localhost:8866"
+const baseUrl = "http://localhost:9966"
 // 未授权用户
 
 export const Login = baseUrl + '/api/login';
