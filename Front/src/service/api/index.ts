29e4e166a3c2aa814704c86a6f44143daf7a0c2e
linshuizhaoying <4799109@qq.com>
2017-12-11 19:08:58 +0800

    fix 消息中心 Front Build

--- Front/src/service/api/index.ts
+++ Front/src/service/api/index.ts
@@ -1,8 +1,8 @@
 // 定义测试接口地址
 // const baseUrl = "http://new.haoqiao.me:8866"
 // 部署地址
-// const baseUrl = "http://wmock.haoqiao.me:9966"
-const baseUrl = "http://localhost:9966"
+const baseUrl = "http://wmock.haoqiao.me:9966"
+// const baseUrl = "http://localhost:9966"
 // 未授权用户
 
 export const Login = baseUrl + '/api/login';
