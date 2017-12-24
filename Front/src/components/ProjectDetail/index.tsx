7989959e8970f4c2ef91be294f366d1699519b3b
linshuizhaoying <4799109@qq.com>
2017-12-24 22:00:31 +0800

    feat 项目示例

---
+++ Front/src/components/ProjectDetail/index.tsx
@@ -0,0 +1,175 @@
+import * as React from 'react';
+import './index.less';
+import Tabs from 'antd/lib/tabs';
+import Input from 'antd/lib/input';
+import Icon from 'antd/lib/icon';
+import Button from 'antd/lib/button';
+import Tooltip from 'antd/lib/tooltip';
+import Avatar from 'antd/lib/avatar';
+import Popover from 'antd/lib/popover';
+
+const TabPane = Tabs.TabPane;
+
+class EditableCell extends React.Component<any, any> {
+  state = {
+    value: this.props.value,
+    editable: false,
+  }
+  handleChange = (e: any) => {
+    const value = e.target.value;
+    this.setState({ value });
+  }
+  check = () => {
+    this.setState({ editable: false });
+    if (this.props.onChange) {
+      this.props.onChange(this.state.value);
+    }
+  }
+  edit = () => {
+    this.setState({ editable: true });
+  }
+  render() {
+    const { value, editable } = this.state;
+    return (
+      <div className="editable-cell">
+        {
+          editable ?
+            <div className="editable-cell-input-wrapper">
+              <Input
+                value={value}
+                onChange={this.handleChange}
+                onPressEnter={this.check}
+              />
+              <Icon
+                type="check"
+                className="editable-cell-icon-check"
+                onClick={this.check}
+              />
+            </div>
+            :
+            <div className="editable-cell-text-wrapper">
+              {value || ' '}
+              <Icon
+                type="edit"
+                className="editable-cell-icon"
+                onClick={this.edit}
+              />
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+
+export class ProjectDetail extends React.Component<any, any> {
+  constructor (props: any) {
+    super(props)
+    this.state = {
+      selectUserId:'', // 从项目选择的用户ID
+    };
+  }
+  componentDidMount() {
+    
+  }
+
+  componentWillReceiveProps(nextProps: any) {
+  }
+  changeProjectName = (id: any, index: any) => {
+    return (value: any) => {
+      console.log(id,index,value)
+      // const dataSource = [...this.state.dataSource];
+      // const target = dataSource.find(item => item.id === id);
+      // if (target) {
+      //   target[dataIndex] = value;
+      //   this.setState({ dataSource });
+      // }
+    };
+  }
+  render () {
+    return(
+      <div id="ProjectDetail">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="项目简介" key="1">
+              <div className="proejctName title">
+                项目名称
+
+                <EditableCell
+                    value='测试项目'
+                    onChange={this.changeProjectName('123asd', 'name')}
+                  />
+              </div>
+              <div className="proejctMockUrl title">
+                Mock地址
+                
+              <Tooltip placement="top" title={'点击复制到粘贴板'}>
+                <Button type="dashed" className="projectUrl">
+                  /wmock/project/xxx123
+                </Button>
+              </Tooltip>
+              
+              </div>
+               <div className="proejctGroup title">
+                团队成员
+                 <ul className="userList">
+                  <li> 
+                    <Popover content={
+                          <div>
+                            <div>
+                              用户名: 2333
+                            </div>
+                            <div>
+                              职位: 前端工程师
+                            </div>
+                          </div>
+                      }>
+                          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    </Popover>   
+                    <div className="projectUserDelete">
+                       <Icon type="close" />
+                    </div>
+                  </li>
+                  
+                  <li> 
+                  <Popover content={
+                        <div>
+                          <div>
+                            用户名: 2333
+                          </div>
+                          <div>
+                            职位: 前端工程师
+                          </div>
+                        </div>
+                    }>
+                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                  </Popover>   
+                  <div className="projectUserDelete">
+                      <Icon type="close" />
+                  </div>
+                </li>
+
+                  <li className="addProjectUser">
+                    <Icon type="plus" />
+                  </li>
+            
+                 </ul>
+              </div>
+              <div className="proejctBeta title">
+                项目版本
+                  <EditableCell
+                    value='V1.0'
+                    onChange={this.changeProjectName('123asd', 'name')}
+                  />
+              </div>
+            </TabPane>
+            <TabPane tab="项目动态" key="2">Content of Tab Pane 2</TabPane>
+          </Tabs>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default ProjectDetail;
--- Front/src/containers/App/DashBoard/ProjectDemo/index.tsx
+++ Front/src/containers/App/DashBoard/ProjectDemo/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import './index.less';
+import Card from 'antd/lib/card';
+import Tree  from 'antd/lib/tree';
+import Icon  from 'antd/lib/icon';
+import Tooltip  from 'antd/lib/tooltip';
+// import Button from 'antd/lib/button';
+import ProjectDetail from '../../../../components/ProjectDetail';
+const TreeNode = Tree.TreeNode;
 
 export class ProjectDemo extends React.Component<any, any> {
   constructor (props: any) {
@@ -15,11 +22,219 @@ export class ProjectDemo extends React.Component<any, any> {
   componentWillReceiveProps(nextProps: any) {
   }
 
+  onSelect = (selectedKeys: any, info: any) => {
+    console.log('selected', selectedKeys, info);
+  }
 
   render () {
     return(
       <div id="ProjectDemo">
-        ProjectDemo
+        <div className="projectTree">
+          <div className="addProject">
+            <Tooltip placement="right" title={'添加项目'}>
+               <Icon type="plus-circle" />
+            </Tooltip>
+           
+          </div>
+          <Card style={{ width: 280 }}>
+
+            <Tree defaultExpandedKeys={['1']} onSelect={this.onSelect}>
+              <TreeNode title={
+                <div className="projectType">
+                  
+                  <div className="projectName"><Icon type="folder-open" />REST接口示例超长字符串测试asd123</div> 
+                  <div className="projectOperate">
+
+                    <Tooltip placement="top" title={'删除项目'}>
+                        <Icon type="delete" className="operate-icon"/>
+                      </Tooltip>
+
+                      <Tooltip placement="top" title={'复制项目'}>
+                        <Icon type="copy" className="operate-icon"/>
+                      </Tooltip>
+
+                      <Tooltip placement="top" title={'导入接口'}>
+                        <Icon type="file-add" className="operate-icon"/>
+                      </Tooltip>
+
+                
+                      
+                  </div>
+                </div>
+              } key="1">
+                  <TreeNode title={ 
+                    <div className="interfaceType">
+                        
+                        <div className="interfaceName"><Icon type="file" /> 获取 </div> 
+                        <div className="interfaceOperate">
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'复制接口'} >
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                              
+                      </div>
+                    </div>
+                  }  key="2" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                
+                        <div className="interfaceName"><Icon type="file" /> 增加 </div>
+                        <div className="interfaceOperate">
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+                              
+
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon" />
+                            </Tooltip>
+
+                        
+                      </div>
+                    </div>
+                  }  key="3" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                  
+                        <div className="interfaceName"><Icon type="file" /> 删除 </div>  
+                        <div className="interfaceOperate">
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                  
+                              
+                      </div>
+                    </div>
+                  }   key="0-0-0-1" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                  
+                        <div className="interfaceName"><Icon type="file" /> 更新 </div>
+                        <div className="interfaceOperate">
+
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                              
+                      </div>
+                    </div>
+                  }  key="0-0-0-2" />
+              </TreeNode>
+              
+              <TreeNode title={
+                <div className="projectType">
+                
+                  <div className="projectName"><Icon type="folder-open" />基本操作接口示例</div>
+                  <div className="projectOperate">
+
+
+                      <Tooltip placement="top" title={'删除项目'}>
+                        <Icon type="delete" className="operate-icon" />
+                      </Tooltip>
+
+                      <Tooltip placement="top" title={'Copy'}>
+                        <Icon type="copy" className="operate-icon"/>
+                      </Tooltip>
+
+                      <Tooltip placement="top" title={'新增接口'}>
+                        <Icon type="file-add" className="operate-icon"/>
+                      </Tooltip>
+
+                      
+                  </div>
+                </div>
+              }  key="8">
+                <TreeNode title={ 
+                    <div className="interfaceType">
+                        
+                        <div className="interfaceName"><Icon type="file" /> 注册</div> 
+                        <div className="interfaceOperate">
+                            <Tooltip placement="top" title={'复制接口'} >
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+                              
+                      </div>
+                    </div>
+                  }  key="9" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                
+                        <div className="interfaceName"><Icon type="file" /> 登录 </div>
+                        <div className="interfaceOperate">
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon" />
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+                              
+                      </div>
+                    </div>
+                  }  key="10" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                  
+                        <div className="interfaceName"><Icon type="file" /> token </div>  
+                        <div className="interfaceOperate">
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+                              
+                      </div>
+                    </div>
+                  }   key="11" />
+                  <TreeNode  title={ 
+                    <div className="interfaceType">
+                  
+                        <div className="interfaceName"><Icon type="file" /> 退出 </div>
+                        <div className="interfaceOperate">
+                            <Tooltip placement="top" title={'复制接口'}>
+                              <Icon type="copy" className="operate-icon"/>
+                            </Tooltip>
+
+                            <Tooltip placement="top" title={'删除接口'}>
+                              <Icon type="delete" className="operate-icon"/>
+                            </Tooltip>
+                              
+                      </div>
+                    </div>
+                  }  key="12" />
+              </TreeNode>
+
+            </Tree>
+        
+          </Card>
+        </div>
+        <div className="projectContent">
+          <ProjectDetail/>
+        </div> 
+        
       </div>
     )
   }
